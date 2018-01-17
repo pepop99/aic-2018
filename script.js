1a23b837b638a5293e923776a059eecc4c3a83a3
kunal-mohta <kunalmohta1818@gmail.com>
2018-01-17 23:22:24 +0530

    update for main

--- script.js
+++ script.js
@@ -1,30 +1,150 @@
-document.getElementsByClassName("menu")[0].addEventListener("click", function(){
-	document.getElementsByClassName("hamburger")[0].style.left = "-10vw";
-});
-
-document.getElementsByClassName("arrow")[0].addEventListener("click", function(){
+function hamClose(){
 	document.getElementsByClassName("hamburger")[0].style.left = "-110vw";
-});
+}
 
+function hamOpen(){
+	document.getElementsByClassName("hamburger")[0].style.left = "-10vw";
+}
 
-document.getElementsByClassName("nav-rules")[0].addEventListener("click", function(){
+document.getElementsByClassName("menu")[0].addEventListener("click", hamOpen);
+document.getElementsByClassName("arrow")[0].addEventListener("click", hamClose);
+
+function rulesClick(){
 	document.getElementsByClassName("home-body")[0].style.left = "100vw";
 	document.getElementsByClassName("contact-body")[0].style.left = "100vw";
+	document.getElementsByClassName("faq-body")[0].style.left = "100vw";
+	document.getElementsByClassName("overview-body")[0].style.left = "100vw";
+	document.getElementsByClassName("problem-body")[0].style.left = "100vw";
+	
+	document.getElementsByClassName("bot-nav")[0].style.top = "78vh";
 
 	setTimeout(function(){
 		document.getElementsByClassName("rules-body")[0].style.left = "0vw";
 		document.getElementsByClassName("home-body")[0].style.left = "100vw";
 		document.getElementsByClassName("contact-body")[0].style.left = "100vw";
-	}, 500);	
-});
+		document.getElementsByClassName("faq-body")[0].style.left = "100vw";
+		document.getElementsByClassName("overview-body")[0].style.left = "100vw";
+		document.getElementsByClassName("problem-body")[0].style.left = "100vw";
 
-document.getElementsByClassName("nav-contact")[0].addEventListener("click", function(){
+		document.getElementsByClassName("bot-nav")[0].style.top = "78vh";
+	}, 500);	
+}
+
+function contactClick(){
 	document.getElementsByClassName("home-body")[0].style.left = "100vw";
 	document.getElementsByClassName("rules-body")[0].style.left = "100vw";
+	document.getElementsByClassName("faq-body")[0].style.left = "100vw";
+	document.getElementsByClassName("overview-body")[0].style.left = "100vw";
+	document.getElementsByClassName("problem-body")[0].style.left = "100vw";
+	
+	document.getElementsByClassName("bot-nav")[0].style.top = "78vh";
 
 	setTimeout(function(){
 		document.getElementsByClassName("contact-body")[0].style.left = "0vw";
 		document.getElementsByClassName("home-body")[0].style.left = "100vw";
 		document.getElementsByClassName("rules-body")[0].style.left = "100vw";
+		document.getElementsByClassName("faq-body")[0].style.left = "100vw";
+		document.getElementsByClassName("overview-body")[0].style.left = "100vw";
+		document.getElementsByClassName("problem-body")[0].style.left = "100vw";
+
+		document.getElementsByClassName("bot-nav")[0].style.top = "78vh";
 	}, 500);	
-});
+}
+
+function overviewClick(){
+	document.getElementsByClassName("home-body")[0].style.left = "100vw";
+	document.getElementsByClassName("rules-body")[0].style.left = "100vw";
+	document.getElementsByClassName("faq-body")[0].style.left = "100vw";
+	document.getElementsByClassName("contact-body")[0].style.left = "100vw";
+	document.getElementsByClassName("problem-body")[0].style.left = "100vw";
+	
+	document.getElementsByClassName("bot-nav")[0].style.top = "78vh";
+
+	setTimeout(function(){
+		document.getElementsByClassName("overview-body")[0].style.left = "0vw";
+		document.getElementsByClassName("home-body")[0].style.left = "100vw";
+		document.getElementsByClassName("rules-body")[0].style.left = "100vw";
+		document.getElementsByClassName("faq-body")[0].style.left = "100vw";
+		document.getElementsByClassName("contact-body")[0].style.left = "100vw";
+		document.getElementsByClassName("problem-body")[0].style.left = "100vw";
+
+		document.getElementsByClassName("bot-nav")[0].style.top = "78vh";
+	}, 500);	
+}
+
+function homeClick(){
+	document.getElementsByClassName("contact-body")[0].style.left = "100vw";
+	document.getElementsByClassName("rules-body")[0].style.left = "100vw";
+	document.getElementsByClassName("faq-body")[0].style.left = "100vw";
+	document.getElementsByClassName("overview-body")[0].style.left = "100vw";
+	document.getElementsByClassName("problem-body")[0].style.left = "100vw";
+	
+	document.getElementsByClassName("bot-nav")[0].style.top = "78vh";
+
+	setTimeout(function(){
+		document.getElementsByClassName("home-body")[0].style.left = "0vw";
+		document.getElementsByClassName("contact-body")[0].style.left = "100vw";
+		document.getElementsByClassName("rules-body")[0].style.left = "100vw";
+		document.getElementsByClassName("faq-body")[0].style.left = "100vw";
+		document.getElementsByClassName("overview-body")[0].style.left = "100vw";
+		document.getElementsByClassName("problem-body")[0].style.left = "100vw";
+
+		document.getElementsByClassName("bot-nav")[0].style.top = "78vh";
+	}, 500);	
+}
+
+function faqClick(){
+	document.getElementsByClassName("contact-body")[0].style.left = "100vw";
+	document.getElementsByClassName("rules-body")[0].style.left = "100vw";
+	document.getElementsByClassName("home-body")[0].style.left = "100vw";
+	document.getElementsByClassName("overview-body")[0].style.left = "100vw";
+	document.getElementsByClassName("problem-body")[0].style.left = "100vw";
+	
+	document.getElementsByClassName("bot-nav")[0].style.top = "78vh";
+
+	setTimeout(function(){
+		document.getElementsByClassName("faq-body")[0].style.left = "0vw";
+		document.getElementsByClassName("contact-body")[0].style.left = "100vw";
+		document.getElementsByClassName("rules-body")[0].style.left = "100vw";
+		document.getElementsByClassName("home-body")[0].style.left = "100vw";
+		document.getElementsByClassName("overview-body")[0].style.left = "100vw";
+		document.getElementsByClassName("problem-body")[0].style.left = "100vw";
+
+		document.getElementsByClassName("bot-nav")[0].style.top = "78vh";
+	}, 500);	
+}
+
+function problemClick(){
+	document.getElementsByClassName("contact-body")[0].style.left = "100vw";
+	document.getElementsByClassName("rules-body")[0].style.left = "100vw";
+	document.getElementsByClassName("home-body")[0].style.left = "100vw";
+	document.getElementsByClassName("overview-body")[0].style.left = "100vw";
+	document.getElementsByClassName("faq-body")[0].style.left = "100vw";
+		
+	document.getElementsByClassName("bot-nav")[0].style.top = "100vh";
+
+	setTimeout(function(){
+		document.getElementsByClassName("problem-body")[0].style.left = "0vw";
+		document.getElementsByClassName("contact-body")[0].style.left = "100vw";
+		document.getElementsByClassName("rules-body")[0].style.left = "100vw";
+		document.getElementsByClassName("home-body")[0].style.left = "100vw";
+		document.getElementsByClassName("overview-body")[0].style.left = "100vw";
+		document.getElementsByClassName("faq-body")[0].style.left = "100vw";
+
+		document.getElementsByClassName("bot-nav")[0].style.top = "100vh";
+	}, 500);	
+}
+
+
+document.getElementsByClassName("nav-rules")[0].addEventListener("click", rulesClick);
+document.getElementsByClassName("nav-contact")[0].addEventListener("click", contactClick);
+document.getElementsByClassName("nav-overview")[0].addEventListener("click", overviewClick);
+document.getElementsByClassName("nav-home")[0].addEventListener("click", homeClick);
+document.getElementsByClassName("nav-faq")[0].addEventListener("click", faqClick);
+document.getElementsByClassName("problems")[0].addEventListener("click", problemClick);
+
+document.getElementsByClassName("ham-rules")[0].addEventListener("click", function(){rulesClick(); hamClose();});
+document.getElementsByClassName("ham-contact")[0].addEventListener("click", function(){contactClick(); hamClose();});
+document.getElementsByClassName("ham-overview")[0].addEventListener("click", function(){overviewClick(); hamClose();});
+document.getElementsByClassName("ham-home")[0].addEventListener("click", function(){homeClick(); hamClose();});
+document.getElementsByClassName("ham-faq")[0].addEventListener("click", function(){faqClick(); hamClose();});
